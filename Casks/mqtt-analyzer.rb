f4c3c35ecd289424d8435f1357acd3224e545197
Philipp Arndt <2f.mail@gmx.de>
2026-04-07 14:50:31 +0200

    Update mqtt-analyzer to v3.3.1

--- Casks/mqtt-analyzer.rb
+++ Casks/mqtt-analyzer.rb
@@ -1,8 +1,8 @@
 cask "mqtt-analyzer" do
-  version "3.3.0"
-  sha256 "eb047b5d58cbaae71a60d6a4710811f08839067b8db509968cad419d77370a69"
+  version "3.3.1"
+  sha256 "ea0d26c1facc6c954e4a5bb81df54ef17a2456e8fd0bf4c2461472f51abd11a9"
 
-  url "https://github.com/philipparndt/mqtt-analyzer/releases/download/v3.3.0/MQTTAnalyzer-v3.3.0.zip"
+  url "https://github.com/philipparndt/mqtt-analyzer/releases/download/v3.3.1/MQTTAnalyzer-v3.3.1.zip"
   name "MQTT Analyzer"
   desc "MQTT client for monitoring and debugging MQTT brokers"
   homepage "https://github.com/philipparndt/mqtt-analyzer"
